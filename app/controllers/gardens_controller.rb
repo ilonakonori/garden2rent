1e9abb77966e91296012e7478d40cfe953e1fc0a
iva mudronja <iva.pilizota@gmail.com>
2020-07-10 17:32:48 +0200

    not really booking controller, more garden controller fix

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,25 +1,24 @@
 class GardensController < ApplicationController
-  before_action :set_garden, only: [:show, :edit, :destroy]
+  skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_garden, only: [:show, :edit, :update, :destroy]
 
   def index
-    @gardens = policy_scope(Garden).order(created_at: :desc) # Iva will follow up
+    @gardens = policy_scope(Garden).order(created_at: :desc) # Iva will follow up - done!
   end
 
   def show
   end
 
   def new
-    @garden = Garden.new
+    @garden = current_user.gardens.new
     authorize @garden
   end
 
   def create
-    @garden = Garden.new(garden_params)
-    @garden.user = current_user
+    @garden = current_user.gardens.new(garden_params)
     authorize @garden
-
     if @garden.save
-      redirect_to @gardens
+      redirect_to gardens_path(@garden)
     else
       render :edit
     end
@@ -28,15 +27,24 @@ class GardensController < ApplicationController
   def edit
   end
 
+  def update
+    @garden.update(garden_params)
+    if @garden.save
+      redirect_to @garden
+    else
+      render :edit
+    end
+  end
+
   def destroy
     @garden.destroy
-    redirect_to garden_url, notice: 'Garden was succsesfully removed!'
+    redirect_to gardens_path, notice: 'Garden was succsesfully removed!'
   end
 
   private
 
   def garden_params
-    params.require(:garden).permit(:title, :description, :location, :price, photos: [])
+    params.require(:garden).permit(:title, :description, :location, :price, :user_id, photos: [])
   end
 
   def set_garden
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,13 +1,13 @@
 class Booking < ApplicationRecord
   belongs_to :garden
   belongs_to :user
-  validates :start_date, :end_date, presence: true
+  validates :start_date, :end_date, :garden_id, :user_id, presence: true
 
-  # added custom method
+  # added custom methods
   validate :start_date_cannot_be_in_the_past, :start_date_cannot_be_greater_than_end_date
 
   def start_date_cannot_be_in_the_past
-    if start_date.present? && start_date < Time.now
+    if start_date.present? && start_date < Date.tomorrow
       errors.add(:start_date, "can't be in the past")
     end
   end
@@ -18,8 +18,8 @@ class Booking < ApplicationRecord
     end
   end
 
+  # wip
   def total
-    days = (end_date - start_date)*(60*60*24*1000*1000)
-    days.floor * price
+    (end_date - start_date).to_i * price
   end
 end
