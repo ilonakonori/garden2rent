e3e350a6527cbcedad1bfff636248d8ff1740107
Patrik Winkler <winkler.patrik@gmail.com>
2020-07-11 15:10:05 +0200

    review model/controller/pundit/view, booking index, creating a review

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :gardens do
     resources :bookings, only: [:new, :create]
-    # resources :reviews, only: [:new, :create]
   end
-  resources :bookings, only: [:index, :show]
+  resources :bookings, only: [:index, :show] do
+      resources :reviews, only: [:new, :create]
+  end
 end
