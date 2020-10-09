ff6e5e645f040020e9249963dcc515d8a302beaa
iva mudronja <iva.pilizota@gmail.com>
2020-10-09 18:08:12 +0200

    improved

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,6 +24,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :short_bio, :photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :short_bio, :photo])
   end
 end
