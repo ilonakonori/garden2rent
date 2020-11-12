bd2123b2ccd9c38f424583a494881e1112e7e65d
Ilona Kozenkovaite <kozenkovaite@gmail.com>
2020-11-12 18:51:46 +0100

    users name changed to faker, emails aligned, profile pics changes to actual ppl

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,7 +23,7 @@ puts "starting seed"
     last_name: Faker::Name.last_name,
     short_bio: "Journalist and travel bug is ready to lift the wings and travel the world.
     You can follow my story @travelbug2020 or take care of my garden while I am away!",
-    email: "gardenBug2rent+2020@gmail.com",
+    email: "gardenBug2rent+1@gmail.com",
     password: "garden2rent")
   puts "user not saved"
     image = URI.open("https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80")
@@ -67,7 +67,7 @@ end
     I like working with people and helping others makes me happy.
     But, I don't like working the soil...so I'm renting this garden!
     Have fun with it!",
-    email: "garden2rent+1@gmail.com",
+    email: "garden2rent+2@gmail.com",
     password: "garden2rent")
   puts "user not saved"
     image = URI.open("https://images.unsplash.com/photo-1474176857210-7287d38d27c6?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80")
@@ -104,14 +104,14 @@ end
 
 #4
 1.times do
-  new_user = User.new(first_name: "Alexis",
+  new_user = User.new(first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     short_bio: "I just finished Le Wagon Web Development Bootcamp and started my own studio.
     Now I'm working day and night and don't have a time to maintain this garden...",
-    email: "garden2rent+alexis@gmail.com",
+    email: "garden2rent+3@gmail.com",
     password: "garden2rent")
   puts "user not saved"
-    image = URI.open("https://avatars0.githubusercontent.com/u/61702366?v=4")
+    image = URI.open("https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80")
     new_user.photo.attach(io: image, filename: "#{new_user.first_name}_1", content_type: 'image/png')
     new_user.save!
     puts "added user with photo!"
@@ -140,16 +140,16 @@ end
 
 #7
 1.times do
-  new_user = User.new(first_name: "Patrik",
+  new_user = User.new(first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
-    email: "garden2rent+patrik@gmail.com",
+    email: "garden2rent+4@gmail.com",
     short_bio: "Product manager and fitness coach, somewhere in the intersection of tech, health/fitness and psychology.
     Atm I'm building little tools myself, working remote for Microsoft as software engineer and doing freelance on the side.
     After my typical day I feel little bit low in energy and don't have much time to maintain garden.
     Still, garden is in good shape, I plant flowers at early spring and watering it once a week.",
     password: "garden2rent")
    puts "user not saved"
-    image = URI.open("https://avatars2.githubusercontent.com/u/15158574?v=4")
+    image = URI.open("https://images.unsplash.com/photo-1581992652564-44c42f5ad3ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80")
     new_user.photo.attach(io: image, filename: "#{new_user.first_name}_1", content_type: 'image/png')
     new_user.save!
     puts "added user with photo!"
@@ -180,7 +180,7 @@ end
 
 #10
 1.times do
-  new_user = User.new(first_name: "Ryan",
+  new_user = User.new(first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     short_bio: "Currently working for Le Wagon in Berlin as lead teacher and developer.
     I used to do freelancing on small web dev projects, but I stoped... I really enjoy teaching and love my job,
@@ -188,7 +188,7 @@ end
     email: "garden2rent+ryan@gmail.com",
     password: "garden2rent")
   puts "user not saved"
-    image = URI.open("https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1569342819/rppyppu4uealcfs4tmmv.jpg")
+    image = URI.open("https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80")
     new_user.photo.attach(io: image, filename: "#{new_user.first_name}_1", content_type: 'image/png')
     new_user.save!
     puts "added user with photo!"
@@ -223,10 +223,10 @@ end
   new_user = User.new(first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     short_bio: "If you need anything we're not far away. But for the most part, we will leave you alone and let you enjoy your visit.",
-    email: "garden2rent+6@gmail.com",
+    email: "garden2rent+5@gmail.com",
     password: "garden2rent")
   puts "user not saved"
-    image = URI.open("https://gardeners.s3.amazonaws.com/p/SUBEE33214_3.jpg")
+    image = URI.open("https://images.unsplash.com/photo-1472711649025-510d7c09a6fb?ixlib=rb-1.2.1&auto=format&fit=crop&w=1959&q=80")
     new_user.photo.attach(io: image, filename: "#{new_user.first_name}_1", content_type: 'image/png')
     new_user.save!
     puts "added user with photo!"
@@ -257,14 +257,14 @@ end
 
 #19
 1.times do
-  new_user = User.new(first_name: "Rhett",
+  new_user = User.new(first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     short_bio: "Coming from a finance/banking-background, I took over the operations for Le Wagon in Berlin early 2019.
     Now updating the definition of education for the German market.",
-    email: "garden2rent+7@gmail.com",
+    email: "garden2rent+6@gmail.com",
     password: "garden2rent")
   puts "user not saved"
-    image = URI.open("https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1566543641/yxntliggg3bvxuphgvye.jpg")
+    image = URI.open("https://images.unsplash.com/photo-1508908324153-d1a219719814?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80")
     new_user.photo.attach(io: image, filename: "#{new_user.first_name}_1", content_type: 'image/png')
     new_user.save!
     puts "added user with photo!"
@@ -292,15 +292,15 @@ end
 
 #22
 1.times do
-  new_user = User.new(first_name: "Tom",
+  new_user = User.new(first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     short_bio: "Hi! We’re Kitty Garden Rental, team of four kitties that helps you rest easy when you rent a private, professionally mowed garden from us.
     We promise your garden will be green, safe, and true to what you saw on Garden2Rent or we'll make it right.
     Check-ins are always smooth, and we're here 24/7 to answer any questions or help you find the perfect garden.",
-    email: "garden2rent+8@gmail.com",
+    email: "garden2rent+7@gmail.com",
     password: "garden2rent")
   puts "user not saved"
-    image = URI.open("https://ichef.bbci.co.uk/news/976/cpsprodpb/41CF/production/_109474861_angrycat-index-getty3-3.jpg")
+    image = URI.open("https://images.unsplash.com/photo-1521510186458-bbbda7aef46b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1101&q=80")
     new_user.photo.attach(io: image, filename: "#{new_user.first_name}_1", content_type: 'image/png')
     new_user.save!
     puts "added user with photo!"
@@ -330,14 +330,14 @@ end
 
 #25
 1.times do
-  new_user = User.new(first_name: "Miriam",
+  new_user = User.new(first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     short_bio: "Berliner and movie producer. At the moment working on a new project and most of the time I am not in town.
     My cats Sibilie and Suzi is an EXTRA when it comes to renting flower garden.",
-    email: "garden2rent+9@gmail.com",
+    email: "garden2rent+8@gmail.com",
     password: "garden2rent")
   puts "user not saved"
-    image = URI.open("https://images-na.ssl-images-amazon.com/images/I/71uql8CC6oL._AC_SX466_.jpg")
+    image = URI.open("https://images.unsplash.com/photo-1512663150964-d8f43c899f76?ixlib=rb-1.2.1&auto=format&fit=crop&w=2056&q=80")
     new_user.photo.attach(io: image, filename: "#{new_user.first_name}_1", content_type: 'image/png')
     new_user.save!
     puts "added user with photo!"
@@ -369,14 +369,15 @@ end
 
 #28
 1.times do
-  new_user = User.new(first_name: "Laura",
+  new_user = User.new(first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
-    short_bio: "Hello soil! It's Laura! I like soil, birds and poetry.
-    I'm nice and friendly person.",
-    email: "garden2rent+10@gmail.com",
+    short_bio: "Hello soil! It's me, the village girl who loves her garden! 
+    I love spending time in the nature and poetry.
+    I'm nice and lovely person.",
+    email: "garden2rent+9@gmail.com",
     password: "garden2rent")
   puts "user not saved"
-    image = URI.open("https://i.redd.it/ye2xwh3m94741.jpg")
+    image = URI.open("https://images.unsplash.com/photo-1593202265280-6fb32683dda8?ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80")
     new_user.photo.attach(io: image, filename: "#{new_user.first_name}_1", content_type: 'image/png')
     new_user.save!
     puts "added user with photo!"
@@ -407,14 +408,14 @@ end
 
   #ILONA!
   1.times do
-  new_user = User.new(first_name: "Ilona",
+  new_user = User.new(first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     short_bio: "My Husband and I own this beautiful garden, but unfortunately we don't have a time to maintain it.
     We both work as a software engineers, and travel a lot. As a matter of fact, we are digital nomads.",
-    email: "garden2rent+ilona@gmail.com",
+    email: "garden2rent+10@gmail.com",
     password: "garden2rent")
   puts "user not saved"
-    image = URI.open("https://avatars2.githubusercontent.com/u/61380207?v=4")
+    image = URI.open("https://images.unsplash.com/photo-1543059583-04b5b0950e9a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1933&q=80")
     new_user.photo.attach(io: image, filename: "#{new_user.first_name}_1", content_type: 'image/png')
     new_user.save!
     puts "added user with photo!"
@@ -456,14 +457,14 @@ end
 
 #13
 1.times do
-  new_user = User.new(first_name: "Dalia",
+  new_user = User.new(first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     short_bio: "This hidden gem is our only Garden2Rent. We love this garden and we are happy to have the chance to share this place with others.
     We also enjoy staying at other gardens when we can. It is such a great way to enjoy a new flowers!",
-    email: "garden2rent+5@gmail.com",
+    email: "garden2rent+11@gmail.com",
     password: "garden2rent")
   puts "user not saved"
-    image = URI.open("https://www.degroot-inc.com/wp-content/uploads/2018/02/Dahlia_Edinburgh_detail.jpg")
+    image = URI.open("https://images.unsplash.com/photo-1504263133569-8d3f2301174e?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80")
     new_user.photo.attach(io: image, filename: "#{new_user.first_name}_1", content_type: 'image/png')
     new_user.save!
     puts "added user with photo!"
