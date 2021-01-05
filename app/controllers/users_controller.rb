3f9e08ff77db3904c69a551145465683f8c28a6d
iva mudronja <iva.pilizota@gmail.com>
2021-01-06 00:46:26 +0100

    change users#show path => users/:first_name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show]
   def show
-    @user = User.find(params[:id])
+    @user = User.find_by(first_name: params[:first_name])
     authorize @user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'pages#home' #/:Small but Cozy Garden Close to Tempelhofer Feld'
+
+  #resources :gardens, only: :show, param: :name
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :gardens do
     collection do
@@ -11,5 +13,6 @@ Rails.application.routes.draw do
   resources :bookings, only: [:index, :show] do
       resources :reviews, only: [:new, :create]
   end
-  resources :users, only: [:show]
+  resources :users, only: :show, param: :first_name
+  #get 'users/:first_name', :to => 'users#show'
 end
