afe4a2860cff8c754939a7520f275ffb7bef91c3
iva mudronja <iva.pilizota@gmail.com>
2021-01-06 02:07:33 +0100

    users#show path

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home' #/:Small but Cozy Garden Close to Tempelhofer Feld'
 
-  #resources :gardens, only: :show, param: :name
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #resources :gardens, only: :show, param: :title
+
   resources :gardens do
     collection do
       get :my_gardens
